4aaa4f13be34495f431d721fc08ae87382015f3a
Devesh Singh <devesh.nttdata@gmail.com>
2018-06-11 00:30:11 +0530

    Added a filter for sub object

--- excel-json_converter/src/main/java/com/devesh/coverter/service/ConverterServiceImpl.java
+++ excel-json_converter/src/main/java/com/devesh/coverter/service/ConverterServiceImpl.java
@@ -15,6 +15,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import com.devesh.coverter.domain.Provider;
+import com.devesh.coverter.domain.ProviderNetworkInformation;
 
 @Service
 public class ConverterServiceImpl implements ConverterService{
@@ -38,15 +39,32 @@ public class ConverterServiceImpl implements ConverterService{
 			List<Row> listRow = IteratorUtils.toList(sheet.iterator());
 			
 			listRow.stream()
-			.filter(s->s.getCell(4)!=null && s.getCell(4).getCellType()==XSSFCell.CELL_TYPE_NUMERIC)
+			.filter(s->(s.getCell(4)!=null && s.getCell(4).getCellType()==XSSFCell.CELL_TYPE_NUMERIC)||
+					(s.getCell(4)==null && s.getCell(5).getCellType()!=XSSFCell.CELL_TYPE_STRING))
+			//.filter(s->s.getCell(4)==null && s.getCell(5).getCellType()!=XSSFCell.CELL_TYPE_STRING)
 			.forEach(s->{
-				Provider provider = new Provider();
-				provider.setCorpEntCd(s.getCell(3)!=null?s.getCell(3).getStringCellValue():"");
-				provider.setPfin(s.getCell(4)!=null?s.getCell(4).getNumericCellValue():0.0);
-				provider.setPfinStartDate(s.getCell(5)!=null?s.getCell(5).getDateCellValue():null);
-				provider.setPfinEndDate(s.getCell(6)!=null?s.getCell(6).getDateCellValue():null);
-				//provider.setPfinStartDate(pfinStartDate);
-				listProvider.add(provider);
+				if(s.getCell(4)!=null){
+					Provider provider = new Provider();
+					provider.setCorpEntCd(s.getCell(3)!=null?s.getCell(3).getStringCellValue():"");
+					provider.setPfin(s.getCell(4)!=null?s.getCell(4).getNumericCellValue():0.0);
+					provider.setPfinStartDate(s.getCell(5)!=null?s.getCell(5).getDateCellValue():null);
+					provider.setPfinEndDate(s.getCell(6)!=null?s.getCell(6).getDateCellValue():null);
+					//provider.setPfinStartDate(pfinStartDate);
+					listProvider.add(provider);
+				}
+				if(s.getCell(4)==null && s.getCell(5).getCellType()!=XSSFCell.CELL_TYPE_STRING){
+					ProviderNetworkInformation providerNetworkInfo = new ProviderNetworkInformation();
+					providerNetworkInfo.setNetworkEffDate(s.getCell(5)!=null?s.getCell(5).getDateCellValue():null);
+					providerNetworkInfo.setNetworkEndDate(s.getCell(6)!=null?s.getCell(6).getDateCellValue():null);
+					providerNetworkInfo.setPnc(s.getCell(7)!=null?s.getCell(7).getStringCellValue():"");
+					if(listProvider.get(listProvider.size()-1).getListProviderNetworkInfo()==null){
+						List<ProviderNetworkInformation> listProviderNetworkInfo = new ArrayList<>();
+						listProviderNetworkInfo.add(providerNetworkInfo);
+						listProvider.get(listProvider.size()-1).setListProviderNetworkInfo(listProviderNetworkInfo);
+					}else{
+						listProvider.get(listProvider.size()-1).getListProviderNetworkInfo().add(providerNetworkInfo);
+					}
+				}
 			});;
 			
 			
