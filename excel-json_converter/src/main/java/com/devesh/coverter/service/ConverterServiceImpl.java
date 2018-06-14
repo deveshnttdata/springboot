f208e086759ecad7224bb6dfb0821f8cc124829f
Devesh Singh <devesh.nttdata@gmail.com>
2018-06-14 22:52:23 +0530

    Revert "Revert "Revert "Initial Commit for exel to json cne""" Reverted This reverts commit 2871a8f99a1a0a1af4aa7f65014f120c9af8e5c1.

--- excel-json_converter/src/main/java/com/devesh/coverter/service/ConverterServiceImpl.java
+++ excel-json_converter/src/main/java/com/devesh/coverter/service/ConverterServiceImpl.java
@@ -1,11 +1,8 @@
 package com.devesh.coverter.service;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,16 +26,12 @@ public class ConverterServiceImpl implements ConverterService{
 		List<Provider> listProvider = new ArrayList<>();
 		
 		
-		//ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-		//File file = new File(classLoader.getResource(excelPath).toString());
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		File file = new File(classLoader.getResource(excelPath).toString());
 		//ClassPathResource resource = new ClassPathResource(excelPath);
 		
-		File file = new File(excelPath);
-		
-		DateFormat df = new SimpleDateFormat("MM-dd-YYYY");
-		
-		try( InputStream inputStream = new FileInputStream(excelPath);
-				//InputStream inputStream = ConverterServiceImpl.class.getClassLoader().getResourceAsStream(excelPath);
+		try( //InputStream inputStream = new FileInputStream(file);
+				InputStream inputStream = ConverterServiceImpl.class.getClassLoader().getResourceAsStream(excelPath);
 				Workbook workbook = new XSSFWorkbook(inputStream)){
 			
 			Sheet sheet = workbook.getSheetAt(0);
@@ -54,15 +47,15 @@ public class ConverterServiceImpl implements ConverterService{
 					Provider provider = new Provider();
 					provider.setCorpEntCd(s.getCell(3)!=null?s.getCell(3).getStringCellValue():"");
 					provider.setPfin(s.getCell(4)!=null?s.getCell(4).getNumericCellValue():0.0);
-					provider.setPfinStartDate(s.getCell(5)!=null?df.format(s.getCell(5).getDateCellValue()):null);
-					provider.setPfinEndDate(s.getCell(6)!=null?df.format(s.getCell(6).getDateCellValue()):null);
+					provider.setPfinStartDate(s.getCell(5)!=null?s.getCell(5).getDateCellValue():null);
+					provider.setPfinEndDate(s.getCell(6)!=null?s.getCell(6).getDateCellValue():null);
 					//provider.setPfinStartDate(pfinStartDate);
 					listProvider.add(provider);
 				}
 				if(s.getCell(4)==null && s.getCell(5).getCellType()!=XSSFCell.CELL_TYPE_STRING){
 					ProviderNetworkInformation providerNetworkInfo = new ProviderNetworkInformation();
-					providerNetworkInfo.setNetworkEffDate(s.getCell(5)!=null?df.format(s.getCell(5).getDateCellValue()):null);
-					providerNetworkInfo.setNetworkEndDate(s.getCell(6)!=null?df.format(s.getCell(6).getDateCellValue()):null);
+					providerNetworkInfo.setNetworkEffDate(s.getCell(5)!=null?s.getCell(5).getDateCellValue():null);
+					providerNetworkInfo.setNetworkEndDate(s.getCell(6)!=null?s.getCell(6).getDateCellValue():null);
 					providerNetworkInfo.setPnc(s.getCell(7)!=null?s.getCell(7).getStringCellValue():"");
 					if(listProvider.get(listProvider.size()-1).getListProviderNetworkInfo()==null){
 						List<ProviderNetworkInformation> listProviderNetworkInfo = new ArrayList<>();
