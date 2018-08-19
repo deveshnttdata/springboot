852ee12b04b95e7cd52b46b58e68170759451129
Devesh Singh <devesh.nttdata@gmail.com>
2018-08-20 00:47:35 +0530

    Added the Summary Repository

---
+++ claims-service/src/main/java/com/devesh/claims/repository/SummaryRepository.java
@@ -0,0 +1,9 @@
+package com.devesh.claims.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devesh.claims.model.Summary;
+
+public interface SummaryRepository extends JpaRepository<Summary,Long>{
+
+}
