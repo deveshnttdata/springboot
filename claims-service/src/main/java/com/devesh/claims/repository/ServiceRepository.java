c01efc20dbbe0cb27948f90065b09e0a5fb6bf10
Devesh Singh <devesh.nttdata@gmail.com>
2018-08-20 02:13:45 +0530

    Updates

--- claims-service/src/main/java/com/devesh/claims/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devesh.claims.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.devesh.claims.model.Service;
-
-public interface ServiceRepository extends JpaRepository<Service, Integer>{
-
-}
